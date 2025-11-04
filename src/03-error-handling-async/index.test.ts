b0278d4b04b451e999bb6b9879137895e9608c85
zagorky <zagorkiynahyi@gmail.com>
2025-11-04 13:46:50 +0300

    refactor: remove tests

--- src/03-error-handling-async/index.test.ts
+++ src/03-error-handling-async/index.test.ts
@@ -12,51 +12,29 @@ describe('resolveValue', () => {
     const result = await resolveValue(value);
     expect(result).toBe(value);
   });
-
-  test('should resolve with different types', async () => {
-    const values = [123, null, undefined, { key: 'value' }, [1, 2, 3]];
-
-    for (const value of values) {
-      await expect(resolveValue(value)).resolves.toBe(value);
-    }
-  });
 });
 
 describe('throwError', () => {
-  test('should throw error when called', () => {
-    expect(() => throwError()).toThrow();
-  });
-
-  test('should throw error with message', () => {
+  test('should throw error with provided message', () => {
     const customMessage = 'Custom error message';
     expect(() => throwError(customMessage)).toThrow(customMessage);
   });
 
-  test('should throw error with default message', () => {
+  test('should throw error with default message if message is not provided', () => {
     expect(() => throwError()).toThrow('Oops!');
-  });
-
-  test('should throw an instance of Error', () => {
-    expect(() => throwError()).toThrow(Error);
   });
 });
 
 describe('throwCustomError', () => {
-  test('should throw custom error when called', () => {
+  test('should throw custom error', () => {
     expect(throwCustomError).toThrow(MyAwesomeError);
-  });
-
-  test('should throw custom error with message', () => {
     expect(throwCustomError).toThrow('This is my awesome custom error!');
   });
 });
 
 describe('rejectCustomError', () => {
-  test('should reject with custom error', async () => {
+  test('should reject custom error', async () => {
     await expect(rejectCustomError()).rejects.toThrow(MyAwesomeError);
-  });
-
-  test('should reject with specific error message', async () => {
     await expect(rejectCustomError()).rejects.toThrow(
       'This is my awesome custom error!',
     );
